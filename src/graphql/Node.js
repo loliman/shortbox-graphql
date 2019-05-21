b176f4b04b8a8fae76be1916cc7c207c4af81d21
christian.riese <christian.riese@dieboldnixdorf.com>
2019-05-21 14:42:31 +0200

    speed up the search

--- src/graphql/Node.js
+++ src/graphql/Node.js
@@ -39,60 +39,64 @@ export const resolvers = {
                 })
             });
 
-            let series = await models.Series.findAll({
-                attributes: [
-                    [Sequelize.fn("concat", Sequelize.col('title'), ' (Vol.', Sequelize.col('volume'), ')'), 'concatinated'],
-                    'volume', 'title', 'startyear', 'endyear', 'fk_publisher'],
-                where: {
-                    '$Publisher.original$': us ? 1 : 0,
-                },
-                having: {
-                    concatinated: {[Sequelize.Op.like]: pattern}
-                },
-                order: [['title', 'ASC'], ['volume', 'ASC']],
-                include: [models.Publisher]
-            });
+            if (res.length < 25) {
+                let series = await models.Series.findAll({
+                    attributes: [
+                        [Sequelize.fn("concat", Sequelize.col('title'), ' (Vol.', Sequelize.col('volume'), ')'), 'concatinated'],
+                        'volume', 'title', 'startyear', 'endyear', 'fk_publisher'],
+                    where: {
+                        '$Publisher.original$': us ? 1 : 0,
+                    },
+                    having: {
+                        concatinated: {[Sequelize.Op.like]: pattern}
+                    },
+                    order: [['title', 'ASC'], ['volume', 'ASC']],
+                    include: [models.Publisher]
+                });
 
-            await asyncForEach(series, async s => {
-                res.push({
-                    type: 'Series',
-                    label: await generateLabel(s),
-                    url: await generateUrl(s, us)
-                })
-            });
+                await asyncForEach(series, async s => {
+                    res.push({
+                        type: 'Series',
+                        label: await generateLabel(s),
+                        url: await generateUrl(s, us)
+                    })
+                });
+            }
 
-            let issues = await models.Issue.findAll({
-                attributes: [[models.sequelize.fn('MIN', models.sequelize.col('Issue.title')), 'title'],
-                    [models.sequelize.fn('MIN', models.sequelize.col('format')), 'format'],
-                    [models.sequelize.fn('MIN', models.sequelize.col('variant')), 'variant'],
-                    [Sequelize.fn("concat", Sequelize.col('Series.title'), ' (Vol.', Sequelize.col('Series.volume'), ') #', Sequelize.col('number'), ' (', Sequelize.col('format'), ')'), 'concatinated'],
-                    'id', 'number', 'fk_series'],
-                where: {
-                    '$Series->Publisher.original$': us ? 1 : 0
-                },
-                group: ['fk_series', 'number'],
-                having: {
-                    concatinated: {[Sequelize.Op.like]: pattern}
-                },
-                order: [['number', 'ASC'], ['variant', 'DESC'], ['title', 'DESC'], ['format', 'DESC']],
-                grgenerateUrloup: ['fk_series', 'number'],
-                include: [
-                    {
-                        model: models.Series,
-                        include: [
-                            models.Publisher
-                        ]
-                    }
-                ]
-            });
+            if (res.length < 25) {
+                let issues = await models.Issue.findAll({
+                    attributes: [[models.sequelize.fn('MIN', models.sequelize.col('Issue.title')), 'title'],
+                        [models.sequelize.fn('MIN', models.sequelize.col('format')), 'format'],
+                        [models.sequelize.fn('MIN', models.sequelize.col('variant')), 'variant'],
+                        [Sequelize.fn("concat", Sequelize.col('Series.title'), ' (Vol.', Sequelize.col('Series.volume'), ') #', Sequelize.col('number'), ' (', Sequelize.col('format'), ')'), 'concatinated'],
+                        'id', 'number', 'fk_series'],
+                    where: {
+                        '$Series->Publisher.original$': us ? 1 : 0
+                    },
+                    group: ['fk_series', 'number'],
+                    having: {
+                        concatinated: {[Sequelize.Op.like]: pattern}
+                    },
+                    order: [['number', 'ASC'], ['variant', 'DESC'], ['title', 'DESC'], ['format', 'DESC']],
+                    grgenerateUrloup: ['fk_series', 'number'],
+                    include: [
+                        {
+                            model: models.Series,
+                            include: [
+                                models.Publisher
+                            ]
+                        }
+                    ]
+                });
 
-            await asyncForEach(issues, async i => {
-                res.push({
-                    type: 'Issue',
-                    label: await generateLabel(i),
-                    url: await generateUrl(i, us)
-                })
-            });
+                await asyncForEach(issues, async i => {
+                    res.push({
+                        type: 'Issue',
+                        label: await generateLabel(i),
+                        url: await generateUrl(i, us)
+                    })
+                });
+            }
 
             res = matchSorter(res, orgiginalPattern, {keys: ['label']});
             if (res.length >= 25)
