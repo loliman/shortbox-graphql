c68267a3f64b4072ce25c53f94ccd22bb3d744f1
Christian Riese <christian.riese@dieboldnixdorf.com>
2020-02-19 08:46:15 +0100

    improved quick search

--- src/graphql/Node.js
+++ src/graphql/Node.js
@@ -19,62 +19,73 @@ export const resolvers = {
             if (!offset)
                 offset = 0;
 
-            pattern = '%' + pattern.replace(/\s/g, '%') + '%';
-            let res = await models.sequelize.query("SELECT type, \n" +
-               "       Createlabel(type, name, title, volume, startyear, endyear, number, format, variant) AS label, \n" +
-               "       Createurl(type, original, name, title, volume, number, format, variant) AS url \n" +
-               "FROM   (SELECT \"publisher\" AS type, \n" +
-               "               original    AS original, \n" +
-               "               name        AS name, \n" +
-               "               \"\"          AS title, \n" +
-               "               0           AS volume, \n" +
-               "               0           AS startyear, \n" +
-               "               0           AS endyear, \n" +
-               "               0           AS number, \n" +
-               "               ''          AS format, \n" +
-               "               ''          AS variant \n" +
-               "        FROM   publisher p \n" +
-               "        WHERE  original = " + (us ? 1 : 0) + " \n" +
-               "        UNION \n" +
-               "        SELECT \"series\"    AS type, \n" +
-               "               original    AS original, \n" +
-               "               name        AS name, \n" +
-               "               s.title     AS title, \n" +
-               "               volume      AS volume, \n" +
-               "               s.startyear AS startyear, \n" +
-               "               s.endyear   AS endyear, \n" +
-               "               0           AS number, \n" +
-               "               ''          AS format, \n" +
-               "               ''          AS variant \n" +
-               "        FROM   series s \n" +
-               "               LEFT JOIN publisher p \n" +
-               "                      ON s.fk_publisher = p.id \n" +
-               "        WHERE  p.original = " + (us ? 1 : 0) + " \n" +
-               "        UNION \n" +
-               "        SELECT \"issue\"     AS type, \n" +
-               "               original    AS original, \n" +
-               "               name        AS name, \n" +
-               "               s.title     AS title, \n" +
-               "               volume      AS volume, \n" +
-               "               s.startyear AS startyear, \n" +
-               "               s.endyear   AS endyear, \n" +
-               "               number      AS number, \n" +
-               "               format      AS format, \n" +
-               "               variant     AS variant \n" +
-               "        FROM   issue i \n" +
-               "               LEFT JOIN series s \n" +
-               "                      ON i.fk_series = s.id \n" +
-               "               LEFT JOIN publisher p \n" +
-               "                      ON s.fk_publisher = p.id \n" +
-               "        WHERE  p.original = " + (us ? 1 : 0) + " \n" +
-               "        ORDER  BY title, \n" +
-               "                  volume, \n" +
-               "                  name, \n" +
-               "                  Cast(number AS UNSIGNED), \n" +
-               "                  format, \n" +
-               "                  variant) a \n" +
-               "HAVING label LIKE '" + pattern +"' \n" +
-               "LIMIT 25 offset " + offset);
+            if(!pattern || pattern.trim() === "")
+                return [];
+
+            let query = "SELECT * FROM \n" +
+                "(SELECT type, \n" +
+                "       Createlabel(type, name, title, volume, startyear, endyear, number, format, variant) AS label, \n" +
+                "       Createurl(type, original, name, title, volume, number, format, variant) AS url \n" +
+                "FROM   (SELECT \"publisher\" AS type, \n" +
+                "               original    AS original, \n" +
+                "               name        AS name, \n" +
+                "               \"\"          AS title, \n" +
+                "               0           AS volume, \n" +
+                "               0           AS startyear, \n" +
+                "               0           AS endyear, \n" +
+                "               0           AS number, \n" +
+                "               ''          AS format, \n" +
+                "               ''          AS variant \n" +
+                "        FROM   publisher p \n" +
+                "        WHERE  original = " + (us ? 1 : 0) + " \n" +
+                "        UNION \n" +
+                "        SELECT \"series\"    AS type, \n" +
+                "               original    AS original, \n" +
+                "               name        AS name, \n" +
+                "               s.title     AS title, \n" +
+                "               volume      AS volume, \n" +
+                "               s.startyear AS startyear, \n" +
+                "               s.endyear   AS endyear, \n" +
+                "               0           AS number, \n" +
+                "               ''          AS format, \n" +
+                "               ''          AS variant \n" +
+                "        FROM   series s \n" +
+                "               LEFT JOIN publisher p \n" +
+                "                      ON s.fk_publisher = p.id \n" +
+                "        WHERE  p.original = " + (us ? 1 : 0) + " \n" +
+                "        UNION \n" +
+                "        SELECT \"issue\"     AS type, \n" +
+                "               original    AS original, \n" +
+                "               name        AS name, \n" +
+                "               s.title     AS title, \n" +
+                "               volume      AS volume, \n" +
+                "               s.startyear AS startyear, \n" +
+                "               s.endyear   AS endyear, \n" +
+                "               number      AS number, \n" +
+                "               format      AS format, \n" +
+                "               variant     AS variant \n" +
+                "        FROM   issue i \n" +
+                "               LEFT JOIN series s \n" +
+                "                      ON i.fk_series = s.id \n" +
+                "               LEFT JOIN publisher p \n" +
+                "                      ON s.fk_publisher = p.id \n" +
+                "        WHERE  p.original = " + (us ? 1 : 0) + " \n" +
+                "        ORDER  BY title, \n" +
+                "                  volume, \n" +
+                "                  name, \n" +
+                "                  Cast(number AS UNSIGNED), \n" +
+                "                  format, \n" +
+                "                  variant) a \n" +
+                "HAVING label LIKE '%" + pattern.replace(/\s/g, '%') + "%') a \n" +
+                "ORDER BY \n" +
+                "    CASE WHEN label LIKE '" + pattern + "' THEN 1 \n" +
+                "        WHEN label LIKE '" + pattern + "%' THEN 2 \n" +
+                "        WHEN label LIKE '%" + pattern + "' THEN 4 \n" +
+                "        ELSE 3 \n" +
+                "    END ASC, label ASC \n" +
+                "LIMIT 25 offset " + offset;
+
+            let res = await models.sequelize.query(query);
 
             return res[0];
         }
