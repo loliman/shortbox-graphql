467ab39a5d20c6ad41ebb76631b24f24e9b4f810
Christian Riese <christian.riese@hlag.com>
2026-02-16 13:54:14 +0100

    chore: remove legacy graphql and objection code paths

--- test/MarvelFandomCrawler.test.ts
+++
@@ -1,8 +0,0 @@
-import {MarvelFandomCrawler} from '../src/core/crawler/MarvelFandomCrawler';
-
-test('Simple crawler test', async () => {
-  let crawler: MarvelFandomCrawler = new MarvelFandomCrawler();
-  let issue = await crawler.crawl('17', 'Iron Man', 5);
-
-  console.log(issue);
-});
